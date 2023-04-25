18961aa5f6d45e04ba87c4b76d636c0d8c641f44
iuliabadeaa <mariaiulia.badea@ulbsibiu.ro>
2023-04-25 15:39:51 +0300

    function andgate doesn't work

--- gui.py
+++ gui.py
@@ -6,7 +6,13 @@ root.title("Interface")
 root.geometry("600x600")
 
 
-
+# def if_not_zero_or_one():
+#     if int(entry1.get())!=0 or int(entry1.get())!=1:
+#         root.destroy()
+#         error_root=Tk()
+#         error_root.title("Error!")
+#         error_label=Label(error_root,text="Input must be 0 or 1!")
+#         error_label.configure(font="40,red")
 
 description=Label(root,text="AND gate requires at least two inputs: ")
 description.place(x=100,y=100)
@@ -26,11 +32,20 @@ label.place(x=270,y=200)
 def andgate():
     x=int(entry1.get())
     y=int(entry2.get())
-    output=x and y
-    description_label=Label(root,text="Output: ")
-    description_label.place(x=500,y=250)
-    and_label=Label(root,text=bool(output))
-    and_label.place(x=550,y=250)
+    if (x!=0 or x!=1) and (y!=0 or y!=1):
+        root.destroy()
+        error_root=Tk()
+        error_root.title("Error!")
+        error_label=Label(error_root,text="Input must be 0 or 1!")
+        error_label.configure(font="40,red")
+        error_label.place(x=30,y=80)    
+    else:
+        output=x and y
+        description_label=Label(root,text="Output: ")
+        description_label.place(x=500,y=250)
+        and_label=Label(root,text=bool(output))
+        and_label.place(x=550,y=250)
+        
 
     
 
