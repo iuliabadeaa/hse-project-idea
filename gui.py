9fa93b7287131cc90876544f76c1db45974d9f56
iuliabadeaa <mariaiulia.badea@ulbsibiu.ro>
2023-04-25 15:52:26 +0300

    and gate working

--- gui.py
+++ gui.py
@@ -5,15 +5,6 @@ root=Tk()
 root.title("Interface")
 root.geometry("600x600")
 
-
-# def if_not_zero_or_one():
-#     if int(entry1.get())!=0 or int(entry1.get())!=1:
-#         root.destroy()
-#         error_root=Tk()
-#         error_root.title("Error!")
-#         error_label=Label(error_root,text="Input must be 0 or 1!")
-#         error_label.configure(font="40,red")
-
 description=Label(root,text="AND gate requires at least two inputs: ")
 description.place(x=100,y=100)
 input1=Label(root,text="A")
@@ -32,21 +23,29 @@ label.place(x=270,y=200)
 def andgate():
     x=int(entry1.get())
     y=int(entry2.get())
-    if (x!=0 or x!=1) and (y!=0 or y!=1):
-        root.destroy()
-        error_root=Tk()
-        error_root.title("Error!")
-        error_label=Label(error_root,text="Input must be 0 or 1!")
-        error_label.configure(font="40,red")
-        error_label.place(x=30,y=80)    
-    else:
+    if (x==0 or x==1) and (y==0 or y==1):
         output=x and y
         description_label=Label(root,text="Output: ")
         description_label.place(x=500,y=250)
         and_label=Label(root,text=bool(output))
         and_label.place(x=550,y=250)
+
+    else:
+        root.destroy()
+        error_root=Tk()
+        error_root.title("Error!")
+        error_label=Label(error_root,text="Input must be 0 or 1!")
+        error_label.configure(font="40,red")
+        error_label.place(x=30,y=80)
+        mainloop() 
         
 
+def reset():
+    entry1.delete(0,END)
+    entry2.delete(0,END)
+
+resetbutton=Button(root,text="RESET",command=reset)
+resetbutton.place(x=400,y=400)
     
 
 runbutton=Button(root,text="RUN",command=andgate)
